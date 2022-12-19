e2baa338ce6f2a253c48dfbdd9ced2156ff75ec6
Dane Acree <dacree0626@gmail.com>
2022-12-18 20:34:08 -0600

    added tests to sauce demo

--- cypress/e2e/saucedemo.cy.js
+++ cypress/e2e/saucedemo.cy.js
@@ -21,16 +21,43 @@ it('logs in as standard_user and places an order', () =>{
     cy.get('#checkout_complete_container').contains('THANK YOU FOR YOUR ORDER').should('be.visible')
 })
 
-it.only('logs in as standard_user and sorts by price low to high', () =>{
+it('logs in as standard_user and sorts by price low to high', () =>{
     saucePage.navigate('https://www.saucedemo.com/');
     saucePage.enterUserName('standard_user');
     saucePage.enterPassword('secret_sauce');
     saucePage.clickLoginButton();
 
-    cy.get('[data-test="product_sort_container"]').select(2)
-    cy.get('.inventory_list').children().eq(0).parent().within(() => {
-        cy.get(".inventory_item").eq(0).parent().within(() => {
-            cy.get(".inventory_item_name").eq(0).should('have.value', 'Sauce Labs Onesie')
+    cy.get('[data-test="product_sort_container"] option').then(dropDownItems =>{
+        console.log(dropDownItems)
+        const dropdownItemsList = Array.from(dropDownItems, dropdown => dropdown.innerText)
+        console.log(dropdownItemsList)
+        const priceLowToHighIndex = dropdownItemsList.findIndex((item) => {
+            return item == 'Price (low to high)' //frailest part of test, if this text changes the test fails
+        })
+        return priceLowToHighIndex
+    }).then((priceLowToHighIndex) => {
+        cy.get('[data-test="product_sort_container"]').select(priceLowToHighIndex)
     })
-     })
+
+    cy.get('.inventory_item > .inventory_item_description > .pricebar > .inventory_item_price').then($items => {
+        const itemPrices = Array.from($items, item => {
+            return parseFloat(item.innerText.substring(1,item.innerText.length))
+        })
+        const sortedItemPrices = Array.from(itemPrices)
+        sortedItemPrices.sort((a, b) => {
+            return a - b;
+          });
+        expect(itemPrices).to.deep.equal(sortedItemPrices)
     })
+})
+
+it('logs in as a locked_out_user and is warned', () =>{
+    saucePage.navigate('https://www.saucedemo.com/');
+    cy.get('[data-test="error"]').should('not.exist')
+    cy.get('.error-message-container').should('have.text','')
+    saucePage.enterUserName('locked_out_user');
+    saucePage.enterPassword('secret_sauce');
+    saucePage.clickLoginButton();
+    cy.get('[data-test="error"]').should('exist')
+    cy.contains('Epic sadface: Sorry, this user has been locked out.').should('be.visible')
+})
